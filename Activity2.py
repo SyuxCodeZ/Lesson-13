e3e4a9cd4c3af100cc156f961cc11052cc63b3e8
SyuxCodeZ <cuberdytm@gmail.com>
2025-01-29 17:33:00 +0600

    sdfvkjerlijuhgqerfljkuhgqwerfoiugerpiugy

---
+++ Activity2.py
@@ -0,0 +1,10 @@
+def weather_con():
+    print(f"Its Autumn At Around {date}")
+    print(f"{date2} Is My Birthday\n")
+
+date = "May 26"
+date2 = date
+weather_con()
+weather_con()
+weather_con()
+weather_con()
